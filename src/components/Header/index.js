2c611193994f24161abac60195aad28d4c24571d
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2020-04-30 21:23:28 -0300

    Criação de header

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './styles.css'
+
+//Stateless component. Componentes formados apenas por funções, e tem esse
+const Header = () => (
+    <header id="main-header">Informativo CEEB</header>
+); 
+
+export default Header;
