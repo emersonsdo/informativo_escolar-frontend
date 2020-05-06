3ea6e0d23352955399fe1b1499b4e7639647c443
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2020-05-05 22:07:39 -0300

    Update

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import base64 from 'react-native-base64';
 
 
 export default class Main extends Component {
@@ -9,12 +10,23 @@ export default class Main extends Component {
     }
 
     testConection = async () => {
-        const response = await api.post('/auth');/*, { 
-            body: {
+        const username = 'email_de_luisa@provedor.com';
+        const password = '12345';
+
+        const response = await api.post('/auth',
+        {
+            //TODO: Corrigir, mandar em cabeçalho
+            headers:{'Authorization': 'Basic ' + base64.encode(username + ":" + password)}
+        });
+        /*console.log(baseURL);
+        const response = await axios({
+            method: 'POST',
+            url: baseURL + '/auth',
+            data: {
                 'email': 'email_de_luisa@provedor.com',
                 'password': '12345'
             }
-        });*/
+        });*/     
 
         console.log(response);
     }
