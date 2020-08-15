fddbdb90da60737ccf1aea4ef3a730508f81b223
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2020-08-15 10:45:57 -0300

    Ajustes

--- src/pages/parent/index.js
+++ src/pages/parent/index.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import api from '../../services/api';
 
 export default class Parent extends Component {
-
-    logout = () => {
-        localStorage.clear();
-    }
 
     render() {
        return (
            <div className='container'>
                 <h1>Página principal do gerenciamento dos pais</h1>
-                <button className="btn" type="submit" onClick={this.logout}>Sair</button>
             </div>
        );
     }
