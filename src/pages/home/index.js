d83a13b17a98e165f74bc4f4879060f1b02c35de
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2020-08-19 22:15:04 -0300

    Refatoração da home para tela mobile

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -18,13 +18,13 @@ class Home extends Component {
                     <h1>Portal do gestor</h1>
                 </div>
 
-                <div className="container-buttons">
+                <div id="container-buttons">
                     {/* esses botões poderiam ser links a */}
                     <a className="button" id ="btn-news" href="/">Informes & avisos 
                         <span><br></br>boletins, atividades e reuniões</span></a>
                     <a className="button" id ="btn-parents" href="/">Cadastro de Pais 
                         <span><br></br>adicionar, listar, excluir</span></a>
-                    <a className="button" id ="btn-users" href="/">Informes e avisos 
+                    <a className="button" id ="btn-users" href="/">Administração 
                         <span><br></br>delegar funções dentro do portal</span></a>
                     {/* <button className="button" id="btn-news" type="submit">Gerenciar avisos</button> */}
                     {/* <button className="button" id="btn-parents" type="submit">Gerenciar pais/responsáveis</button> */}
